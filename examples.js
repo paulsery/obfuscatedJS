3dcba817c6169528ead81fb0c58c475b7fa6e07e
Paul Sery <1596893+paulsery@users.noreply.github.com>
2023-03-02 11:31:39 -0700

    Create examples.js

---
+++ examples.js
@@ -0,0 +1,12 @@
+function ex0() {
+    let total = 0;
+    for (let i = 0; i < 10; i++) {
+        total += i;
+    }
+    console.log("Total: " + total); 
+}
+
+function ex1() {
+    let total = 0;
+    console.log("Example 1"); 
+}
