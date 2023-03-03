aa9c3ab9cb09db40e67037b700d35c200c5b71c6
Paul Sery <1596893+paulsery@users.noreply.github.com>
2023-03-02 19:18:49 -0700

    Update examples.js

--- examples.js
+++ examples.js
@@ -2,15 +2,18 @@ function ex0() {
     console.log("Hello ");
 };
 
+// https://superuser.com/questions/418121/what-is-javascript-obfuscation-and-why-is-it-a-threat
 function ex1() {
     alert("Hello " + username);
 };
 
+// https://superuser.com/questions/418121/what-is-javascript-obfuscation-and-why-is-it-a-threat
 function ex2() {
     var _0xc5b2=["\x6F\x6E\x6C\x6F\x61\x64", "\x48\x65\x6C\x6C\x6F\x20"];window[_0xc5b2[0]]=
 //    function (){alert(_0xc5b2[1]+username);}
 };
 
+// https://superuser.com/questions/418121/what-is-javascript-obfuscation-and-why-is-it-a-threat
 function ex3() {
     eval(unescape("var%20_0xc5b2%3D%5B%22onload%22%2C%22Hello%20%22%5D%3Bwindow"+
   "%5B_0xc5b2%5B0%5D%5D%3Dfunction%20%28%29%7Balert%28_0xc5b2%5B1%5D+username"+
